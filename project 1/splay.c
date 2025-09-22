aed339bb9528d5f9e25df21f74ea8f08baf2af82
Harry-Linner <linhali@zju.edu.cn>
2025-09-22 22:45:58 +0800

    feat: Add base files for Project1

---
+++ project 1/splay.c
@@ -0,0 +1,245 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+  struct node *left;
+  struct node *right;
+  struct node *parent;
+  int val;
+}node;
+node *createnode(int k)
+{
+    node *new=(node *)malloc(sizeof(node));
+    new->left=NULL;
+    new->right=NULL;
+    new->parent=NULL;
+    new->val=k;
+    return new;
+}
+node *insert(node *newnode,node *root)
+{
+    if(root==NULL)
+    {
+      return newnode;
+    }
+    newnode->parent=root;
+    if(newnode->val>root->val)
+      root->right=insert(newnode,root->right);
+    else if(newnode->val<root->val)
+      root->left=insert(newnode,root->left);
+    return root;
+}
+node *search(int k,node *root)
+{
+	if(!root)
+	  return NULL;
+	if(root->val==k)
+	  return root;
+	if(root->val>k)
+	  return search(k,root->left);
+	return search(k,root->right);
+} 
+void rightrotate(node* root,node *newnode)
+{
+    node *nr=newnode->right;
+    if(root->parent)
+    {
+      node *grandfather=root->parent;
+      if(grandfather->left==root)
+      {
+        grandfather->left=newnode;
+        newnode->parent=grandfather;
+      }
+      else if(grandfather->right==root)
+      {
+        grandfather->right=newnode;
+        newnode->parent=grandfather;
+      }
+    }
+    else newnode->parent=NULL;
+    newnode->right=root;
+    root->parent=newnode;
+    root->left=nr;
+    if(nr) 
+	  nr->parent=root;
+    return ;
+}
+void leftrotate(node* root,node *newnode)
+{
+    node *nl=newnode->left;
+    if(root->parent)
+    {
+      node *grandfather=root->parent;
+      if(grandfather->left==root)
+      {
+        grandfather->left=newnode;
+        newnode->parent=grandfather;
+      }
+      else if(grandfather->right==root)
+      {
+        grandfather->right=newnode;
+        newnode->parent=grandfather;
+      }
+    }
+    else newnode->parent=NULL;
+    newnode->left=root;
+    root->parent=newnode;
+    root->right=nl;
+    if(nl) 
+	  nl->parent=root;
+	return ;
+}
+void splay(node *newnode,node *root)
+{
+    while(1)
+    {
+        node *parent=newnode->parent;
+        if(parent==NULL)
+          break;
+        else
+        {
+            if(parent==root)
+            {
+                if(root->left==newnode)
+                {
+                 // printf("l ");
+                  rightrotate(root,newnode);
+                }
+                else if(root->right==newnode)
+                {
+                 // printf("r ");
+                  leftrotate(root,newnode); 
+                }
+            }
+            else
+            {
+                node *grandparent=parent->parent;
+                if(grandparent->left&&grandparent->left->left==newnode) //zig-zig ll
+                {
+                   //printf("ll ");
+                   rightrotate(grandparent,parent);
+                   rightrotate(parent,newnode);
+                }
+                else if(grandparent->left&&grandparent->left->right==newnode) //zig-zag lr
+                {
+                	//printf("lr ");
+                   leftrotate(parent,newnode);
+                   rightrotate(grandparent,newnode);
+                }
+                else if(grandparent->right&&grandparent->right->right==newnode) //zig-zig rr
+                {
+                	//printf("rr ");
+                    leftrotate(grandparent,parent);
+                    leftrotate(parent,newnode);
+                }
+                else if(grandparent->right&&grandparent->right->left==newnode) //zig-zag rl
+                {
+                //printf("rl ");
+                    rightrotate(parent,newnode);
+                    leftrotate(grandparent,newnode);
+                }
+            }
+        }
+    }
+    return ;
+}
+node * findmax(node *root)
+{
+    if(!root)
+      return NULL;
+    if(root->right)
+      return findmax(root->right);
+    return root;
+}
+node * findmin(node *root)
+{
+    if(!root)
+      return NULL;
+    if(root->left)
+      return findmax(root->left);
+    return root;
+}
+node *delete(node *root)
+{
+    node *max=(node*)malloc(sizeof(node));
+	max=findmax(root->left);
+    if(max)
+    {
+      if(max!=root->left)
+	  {	
+	    max->parent->right=max->left;
+	    if(max->left ) max->left->parent=max->parent;
+	    max->left=root->left;
+		root->left->parent=max;
+      }
+	  max->right=root->right;
+      max->parent=NULL;
+      
+      if(root->right) 
+	    root->right->parent=max;
+	  return max;
+    }
+    node *min=(node*)malloc(sizeof(node));
+    min=findmin(root->right);
+    if(min)
+    {
+
+      if(min!=root->right)
+      {
+	    min->parent->left=min->right;
+	    if(min->right ) min->right->parent=min->parent;
+        min->right=root->right;
+        root->right->parent=min; 
+		
+      }      
+     
+	  min->parent=NULL;
+      min->left=NULL;
+     // root=min;
+	 return min;
+    }
+    return NULL;
+}
+void Traverse(node *root)
+{
+    if(!root) return ;
+    Traverse(root->left);
+    printf("%d ",root->val);
+    Traverse(root->right);
+    return ;
+}
+int main()
+{
+	int i;
+    node *root=(node*)malloc(sizeof(node));
+    node *newnode=(node*)malloc(sizeof(node));
+    node *target=(node*)malloc(sizeof(node));
+    node *target1=(node*)malloc(sizeof(node));
+    root=NULL;
+    newnode=NULL;
+    int n,a[101];
+    char operation[101];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+      scanf("%d %c",&a[i],&operation[i]);      
+	  newnode=createnode(a[i]);
+      if(operation[i]=='i')
+      {
+        root=insert(newnode,root);
+        splay(newnode,root);
+        root=newnode;
+      //  Traverse(root);
+      }
+      else if(operation[i]=='d')
+      {
+      	target=search(a[i],root);
+        splay(target,root);
+        root=target;
+       root=delete(root);
+      //  Traverse(root);
+      }
+      target1=search(3,root);
+    } 
+
+    Traverse(root);
+}
